271b712672f4ec46cade4e68de360f351df484a6
Marcin <machikur2@gmail.com>
2020-04-30 20:03:32 +0100

    first commit

---
+++ src/main/java/com/kodilla/Calkulator.java
@@ -0,0 +1,11 @@
+package com.kodilla;
+
+public class Calkulator {
+
+    public void someMethod(){}
+
+    public static void main(String[] args) {
+        Calkulator calk=new Calkulator();
+        calk.someMethod();
+    }
+}
