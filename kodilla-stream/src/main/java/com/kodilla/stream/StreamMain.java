653309052be6510259c1992e3c517f9e5df255c6
Marcin <machikur2@gmail.com>
2020-05-12 19:46:23 +0100

    ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,14 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
         System.out.println("Welcome to module 7 - Stream");
-
+/*
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
         expressionExecutor.executeExpression(5, 10, (a, b) -> a + b);
         expressionExecutor.executeExpression(5, 10, (a, b) -> a - b);
@@ -23,6 +24,9 @@ public class StreamMain {
         poemBeautifier.beautify("Janusz", (a) -> System.out.println(a.toLowerCase()));
 
         poemBeautifier.beautify("Janusz", PoemBeautifier::getBeauty);
+*/
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 
 }
