fe8c91f09bbf58dec02520a489f6484f13ae24ee
Marcin <machikur2@gmail.com>
2020-05-16 09:56:43 +0100

    Board Task

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -8,13 +8,13 @@ public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage() {
         //Given
-        int[] numbers = {10, 12, 14, 16, 18};
-        Array array = new Array();
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 13, 15, 16, 17, 18, 19, 20};
+
 
         //When
-        double average = array.getAverage(numbers);
+        double average = ArrayOperations.getAverage(numbers);
 
         //Then
-        Assert.assertEquals(14, average, 0);
+        Assert.assertEquals(10.5, average, 0);
     }
 }
