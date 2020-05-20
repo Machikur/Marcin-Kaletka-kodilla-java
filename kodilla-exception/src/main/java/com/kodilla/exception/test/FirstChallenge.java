f0d1045d94c3cfa58357b94f8dbcf01b8ae3a259
Marcin <machikur2@gmail.com>
2020-05-20 18:44:18 +0100

    exeption task

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,31 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println("wynik wynosi: " + result);
+        } catch (ArithmeticException a) {
+            System.out.println("nie można dzielić przez zero");
+        } finally {
+            System.out.println("dziekujemy za skorzystanie z programu");
+        }
+
+    }
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+}
